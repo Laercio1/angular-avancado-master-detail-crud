f048847a214f9100bb4d33a7ec48ee53d981006f
Laercio1 <laerciofernandes772@gmail.com>
2022-03-17 17:04:04 -0300

    improving EntryService

--- Desktop/projeto-pratico/finansys/src/app/pages/entries/shared/entry.service.ts
+++ Desktop/projeto-pratico/finansys/src/app/pages/entries/shared/entry.service.ts
@@ -3,7 +3,7 @@ import { Entry } from './entry.model';
 import { CategoryService } from '../../categories/shared/category.service';
 import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
 import { Observable } from 'rxjs';
-import { flatMap } from 'rxjs/operators';
+import { catchError, flatMap } from 'rxjs/operators';
 import { element } from 'protractor';
 
 @Injectable({
@@ -16,21 +16,21 @@ export class EntryService extends BaseResourceService<Entry> {
   }
 
   create(entry: Entry): Observable<Entry> {
-    return this.categoryService.getById(entry.categoryId).pipe(
-      flatMap(category => {
-        entry.category = category;
-        return super.create(entry)
-      })
-      )
+    return this.setCategoryAndSendToServer(entry, super.create.bind(this));
     }
 
     update(entry: Entry): Observable<Entry> {
-      return this.categoryService.getById(entry.categoryId).pipe(
-        flatMap(category => {
-          entry.category = category;
-          return super.update(entry)
-        })
-        )
+      return this.setCategoryAndSendToServer(entry, super.update.bind(this));
+      }
+
+      private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<Entry>{
+        return this.categoryService.getById(entry.categoryId).pipe(
+          flatMap(category => {
+            entry.category = category;
+            return sendFn(entry)
+          }),
+          catchError(this.handleError)
+        );
       }
 
     }
