2637397ce61d74cef28c9fe17417f5dab2f46fd7
Laercio1 <laerciofernandes772@gmail.com>
2022-03-21 10:49:28 -0300

    imporving Reports

--- Desktop/projeto-pratico/finansys/src/app/pages/reports/reports.module.ts
+++ Desktop/projeto-pratico/finansys/src/app/pages/reports/reports.module.ts
@@ -5,12 +5,15 @@ import { ReportsRoutingModule } from './reports-routing.module';
 import { ReportsComponent } from './reports/reports.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 
+import { ChartModule } from 'primeng/chart';
+
 @NgModule({
   declarations: [ReportsComponent],
   imports: [
     CommonModule,
     ReportsRoutingModule,
-    SharedModule
+    SharedModule,
+    ChartModule
   ]
 })
 export class ReportsModule { }
